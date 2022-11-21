0fc37d61050482dfdd19bc157c1b28cce395c608
isaque.s <isaque.error404@gmail.com>
2022-11-21 18:04:34 -0300

    feat: create file and add bank questions

---
+++ src/pages/api/bankQuestions.ts
@@ -0,0 +1,42 @@
+import QuestionModel from "../../model/question";
+import ResponseModel from "../../model/response";
+
+const questions: QuestionModel[] = [
+  new QuestionModel(306, "Qual bicho transmite a doença de chagas?", [
+    ResponseModel.responseWrong("Abelha"),
+    ResponseModel.responseWrong("Barata"),
+    ResponseModel.responseWrong("Pulga"),
+    ResponseModel.responseRight("Barbeiro"),
+  ]),
+
+  new QuestionModel(
+    202,
+    "Qual fruta é conhecida no Norte e Nordeste como 'jerimum'?",
+    [
+      ResponseModel.responseWrong("Caju"),
+      ResponseModel.responseWrong("Côco"),
+      ResponseModel.responseWrong("Chuchu"),
+      ResponseModel.responseRight("Abóbora"),
+    ]
+  ),
+
+  new QuestionModel(203, "Qual é o coletivo de cães?", [
+    ResponseModel.responseWrong("Manada"),
+    ResponseModel.responseWrong("Alcateia"),
+    ResponseModel.responseWrong("Rebanho"),
+    ResponseModel.responseRight("Matilha"),
+  ]),
+
+  new QuestionModel(
+    204,
+    "Qual é o triângulo que tem todos os lados diferentes?",
+    [
+      ResponseModel.responseWrong("Equilátero"),
+      ResponseModel.responseWrong("Isóceles"),
+      ResponseModel.responseWrong("Trapézio"),
+      ResponseModel.responseRight("Escaleno"),
+    ]
+  ),
+];
+
+export default questions;
