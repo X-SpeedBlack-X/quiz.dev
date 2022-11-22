0f326baec6eaa82d5495dd5c99efe1c362d500ac
isaque.s <isaque.error404@gmail.com>
2022-11-21 22:20:31 -0300

    style: add new colors and font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins, sans-serif"],
+      },
+      colors: {
+        purple: {
+          500: "#6c54d8",
+        },
+        green: {
+          600: "#2baa6d",
+        },
+        red: {
+          600: "#e44a4c",
+        },
+      },
+    },
   },
   plugins: [],
 };
