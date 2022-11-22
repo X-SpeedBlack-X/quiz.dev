c25e04375fb5d8a0d5ebe440108d2931f1639268
isaque.s <isaque.error404@gmail.com>
2022-11-21 22:24:29 -0300

    feat: add state, countdown, styles and responseModel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import { Questions } from "../components/Questions";
+import QuestionModel from "../model/question";
+import ResponseModel from "../model/response";
+
+const questionsMock = new QuestionModel(1, "Qual é a Melhor cor?", [
+  ResponseModel.responseWrong("Verde"),
+  ResponseModel.responseWrong("Roxo"),
+  ResponseModel.responseWrong("Branca"),
+  ResponseModel.responseRight("Preto"),
+]);
+
 export default function Home() {
+  const [question, setQuestion] = useState(questionsMock);
+
+  function onResponse(indice: number) {
+    setQuestion(question.replyWith(indice));
+  }
+
+  function timeUp() {
+    if (!question.answered) {
+      setQuestion(question.replyWith(-1));
+    }
+  }
+
   return (
-    <div>
-      <h1 className="text-3xl text-purple-600">Hello, Quiz.dev</h1>
+    <div className="py-8">
+      <div className="flex justify-center items-center h-screen ">
+        <Questions value={question} onResponse={onResponse} timeUp={timeUp} />
+      </div>
     </div>
   );
 }
